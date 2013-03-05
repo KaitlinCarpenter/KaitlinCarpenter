5786051fa63c9e99857a9ff7ea7811ea10cc7095
Kaitlin Carpenter <kaitlincarpenter@icloud.com>
2013-03-04 21:09:47 -0500

    Changed from sqlite3 to pg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def index
+   @active = :index
   end
 
   def about
+    @active = :about
   end
 
   def contact
+    @active = :contact
   end
 end
