c1ece862d7f1676a62f81128290995d5d5070fee
Kaitlin Carpenter <kaitlincarpenter@icloud.com>
2013-07-21 20:06:15 -0400

    Change pins displayed per page.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.order("created_at desc").page(params[:page]).per_page(6)
+    @pins = Pin.order("created_at desc").page(params[:page]).per_page(10)
 
     respond_to do |format|
       format.html # index.html.erb
